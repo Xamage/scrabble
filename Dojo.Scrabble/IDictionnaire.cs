ebae95658c32987d3f5dbcd3213c2c91e10af7b2
Xamage <xmasclet@gmail.com>
2015-01-20 21:23:07 +0100

    dfgdf

---
+++ Dojo.Scrabble/IDictionnaire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo.Scrabble
+{
+    public interface IDictionnaire
+    {
+        IEnumerable<string> TrouverTousLesMots(Chevalet chevalet);
+
+        IEnumerable<string> TrouverLesMotsLesPlusLongs(Chevalet chevalet);
+
+        IEnumerable<string> TrouverLesMotsLesPlusForts(Chevalet chevalet);
+    }
+}
